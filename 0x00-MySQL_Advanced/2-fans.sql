c076a187e7d46c5edb009fdec9e3d01ff3382e5b
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2023-01-12 17:12:48 +0300

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 --  ranks country origins of bands, ordered by the number of (non-unique)  fans
-SELECT COUNT(fans) as nb_fans, origin FROM metal_bands GROUP BY origin;
+SELECT COUNT(fans) as nb_fans, origin FROM metal_bands GROUP BY origin ORDER BY nb_fans;
