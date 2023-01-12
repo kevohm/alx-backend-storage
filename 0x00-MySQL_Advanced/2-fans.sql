4a1a3a15513293c6a58e9b6feabf69faef0f0e1b
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2023-01-12 17:01:51 +0300

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
---  ranks country origins of bands, ordered by the number of (non-unique) fans
-mysql -u userne -p databasename < https://intranet.alxswe.com/rltoken/uPn947gnZLaa0FJrrAFTGQ
+--  ranks country origins of bands, ordered by the number of (non-unique)  fans
+SELECT COUNT(fans) as nb_fans, origin FROM metal_bands GROUP BY origin;
