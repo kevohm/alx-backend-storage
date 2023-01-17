6562a58357b7e4760f3acec37765a86f7b6f6cd8
kevohm <kevinkbet@gmail.com>
2023-01-17 12:21:32 -0800

    finished task-12

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ returns the list of school having a specific topic """
+
 
 def schools_by_topic(mongo_collection, topic):
     """filter by topi in array"""
-    return mongo_collection.find({"topics":{'$all':[topic]}})
+    return mongo_collection.find({"topics": topic})
