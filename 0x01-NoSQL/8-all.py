0a6c554c5cb87b6737d494d5c8361e9cba5f9b6c
kevohm <kevinkbet@gmail.com>
2023-01-17 12:00:24 -0800

    fixed pycodestyle

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """lists all documents in a collection"""
 
+
 def list_all(mongo_collection):
     """Return an empty list if no document in the collection"""
     if mongo_collection is not None:
