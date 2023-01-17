55cba172671d6202f1b667f762cadd4f4bfc6f81
kevohm <kevinkbet@gmail.com>
2023-01-17 12:16:45 -0800

    fixed pycodestyle

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,6 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
     if mongo_collection is not None:
         return mongo_collection.update_many(
                 {"name": name},
