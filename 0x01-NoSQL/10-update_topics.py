34e0165afb597c68977b30bfe46c1196eabc1d3c
kevohm <kevinkbet@gmail.com>
2023-01-17 12:14:44 -0800

    fixed pycodestyle

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,6 +5,6 @@
 def update_topics(mongo_collection, name, topics):
     if mongo_collection is not None:
         return mongo_collection.update_many(
-                {"name": name}, 
+                {"name": name},
                 {"$set": {"topics": topics}}
                 )
