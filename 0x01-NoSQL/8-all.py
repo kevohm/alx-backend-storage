d1ea801f0a71fa4a371c77b7e306bef49d1109fb
kevohm <kevinkbet@gmail.com>
2023-01-17 11:58:32 -0800

    fixed task-9

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,4 +3,5 @@
 
 def list_all(mongo_collection):
     """Return an empty list if no document in the collection"""
-    return [i for i in mongo_collection.find()]
+    if mongo_collection is not None:
+        return mongo_collection.find()
